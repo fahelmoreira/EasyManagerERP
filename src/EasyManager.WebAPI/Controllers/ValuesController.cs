c4ebb5ba88e4647c2b089b59567e7a72713101b7
Rafael Moreira Pereira <fahelmoreira@me.com>
2018-09-11 13:50:35 -0300

    Api controller

--- src/EasyManager.WebAPI/Controllers/ValuesController.cs
+++ src/EasyManager.WebAPI/Controllers/ValuesController.cs
@@ -6,40 +6,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EasyManager.WebAPI.Controllers
 {
-    [Route("api/[controller]")]
     public class ValuesController : Controller
     {
         // GET api/values
-        [HttpGet]
+        [HttpGet, Route("customer/list")]
         public IEnumerable<string> Get()
         {
-            
+
             return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
