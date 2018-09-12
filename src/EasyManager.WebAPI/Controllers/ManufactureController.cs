b7ebf54b794e9d866cb0dd7c63f71e5c8c9c4dcf
Rafael Moreira Pereira <fahelmoreira@me.com>
2018-09-12 18:48:30 -0300

    Manufacture API

---
+++ src/EasyManager.WebAPI/Controllers/ManufactureController.cs
@@ -0,0 +1,43 @@
+using EasyManager.Application.Interfaces;
+using EasyManager.Application.ViewModels;
+using EasyManager.Domain.Core.Bus;
+using EasyManager.Domain.Core.Notifications;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyManager.WebAPI.Controllers
+{
+    [Route("[Controller]")]
+    public class ManufactureController : ApiController
+    {
+        private readonly IManufactureAppServices _manufacureAppServices;
+
+        protected ManufactureController(
+            IManufactureAppServices manufacureAppServices,
+            INotificationHandler<DomainNotification> notifications, 
+            IMediatorHandler mediator) : base(notifications, mediator)
+        {
+            _manufacureAppServices = manufacureAppServices;
+        }
+
+        [HttpGet("list")]
+        public IActionResult Get()
+        {
+            return Response(_manufacureAppServices.GetAll());
+        }
+        
+        [HttpPost("create")]
+        public IActionResult Post([FromBody] ManufactureViewModel manufacture)
+        {
+            if(!ModelState.IsValid)
+            {
+                NotifyModelStateErrors();
+                return Response(manufacture);
+            }
+
+            _manufacureAppServices.Register(manufacture);
+
+            return Response("Manufacture successfully created");
+        }
+    }
+}
