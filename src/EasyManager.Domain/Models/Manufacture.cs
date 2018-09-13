32a52e922419590dd7aeddf5be9ebd2e35c6bcef
Rafael Moreira Pereira <fahelmoreira@me.com>
2018-09-12 21:13:23 -0300

    Manufacture model update

--- src/EasyManager.Domain/Models/Manufacture.cs
+++ src/EasyManager.Domain/Models/Manufacture.cs
@@ -9,9 +9,24 @@ namespace EasyManager.Domain.Models
         /// </summary>
         public string TradeName { get; set; }
         /// <summary>
+        /// Manufacture's corporate name
+        /// </summary>
+        /// <value></value>
+        public string CorporateName { get; set; }
+        /// <summary>
+        /// Manufacture's name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
         /// Corporate taxpayer's number
         /// </summary>
         public string CorporateTaxpayerId { get; set; }
+        /// <summary>
+        /// Manufacture individual taxpayer number
+        /// </summary>
+        /// <value></value>
+        public string IndividualTaxpayerId { get; set; }
         /// <summary>
         /// Manufacture address
         /// </summary>
@@ -20,5 +35,19 @@ namespace EasyManager.Domain.Models
         /// Manufacture contacts
         /// </summary>
         public string Contacts { get; set; }
+        /// <summary>
+        /// Manufacture main email
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// Manufacture main phone number
+        /// </summary>
+        public string PhoneNumber { get; set; }
+        public bool Active { get; set; }
+
+        public Manufacture()
+        {
+            Active = true;
+        }
     }
 }
