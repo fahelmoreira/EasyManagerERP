a2b375e750ba30b485335a9ae848efef74af7d75
Rafael Moreira Pereira <fahelmoreira@me.com>
2018-09-16 13:15:37 -0300

    IoC

--- src/EasyManager.Infra.Data/Repository/FinancialRepository.cs
+++ src/EasyManager.Infra.Data/Repository/FinancialRepository.cs
@@ -1,7 +1,13 @@
+using EasyManager.Domain.Interfaces;
+using EasyManager.Domain.Models;
+using EasyManager.Infra.Data.Context;
+
 namespace EasyManager.Infra.Data.Repository
 {
-    public class FinancialRepository
+    public class FinancialRepository : Repository<Financial>, IFinancialRepository
     {
-        
+        public FinancialRepository(EasyManagerContext context) : base(context)
+        {
+        }
     }
 }
