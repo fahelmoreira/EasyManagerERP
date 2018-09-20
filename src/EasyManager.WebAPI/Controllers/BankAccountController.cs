45434cd2f67f645d9eef30200ae79ca1c1e9fd84
Rafael Moreira Pereira <fahelmoreira@me.com>
2018-09-20 20:05:35 -0300

    Bank account API

--- src/EasyManager.WebAPI/Controllers/BankAccountController.cs
+++ src/EasyManager.WebAPI/Controllers/BankAccountController.cs
@@ -1,4 +1,6 @@
+using System;
 using EasyManager.Application.Interfaces;
+using EasyManager.Application.ViewModels;
 using EasyManager.Domain.Core.Bus;
 using EasyManager.Domain.Core.Notifications;
 using MediatR;
@@ -9,7 +11,7 @@ namespace EasyManager.WebAPI.Controllers
     [Route("[Controller]")]
     public class BankAccountController : ApiController<IBankAccountAppService>
     {
-        protected BankAccountController(IBankAccountAppService appService, 
+        public BankAccountController(IBankAccountAppService appService, 
                                         INotificationHandler<DomainNotification> notifications, 
                                         IMediatorHandler mediator) : base(appService, notifications, mediator)
         {
@@ -19,6 +21,46 @@ namespace EasyManager.WebAPI.Controllers
         public IActionResult Get()
         {
             return Response(_appService.GetAll());
+        }
+
+        [HttpGet("{Guid:Id}")]
+        public IActionResult Get(Guid id)
+        {
+            return Response(_appService.GetById(id));
+        }
+
+        [HttpPost("create")]
+        public IActionResult Post([FromBody] BankAccountViewModel bankAccount)
+        {
+            if (!ModelState.IsValid)
+            {
+                NotifyModelStateErrors();
+                return Response(bankAccount);
+            }
+
+            _appService.Register(bankAccount);
+
+            return Response("Bank account successfully created");
+        }
+
+        [HttpPost("update")]
+        public IActionResult Put([FromBody] BankAccountViewModel bankAccount)
+        {
+            if (!ModelState.IsValid)
+            {
+                NotifyModelStateErrors();
+                return Response(bankAccount);
+            }
+
+            _appService.Update(bankAccount);
+
+            return Response("Bank account successfully updated");
+        }
+
+        public IActionResult Delete([FromBody] Guid id)
+        {
+             _appService.Remove(id);
+            return Response("Bank account successfully removed");
         }
     }
 }
