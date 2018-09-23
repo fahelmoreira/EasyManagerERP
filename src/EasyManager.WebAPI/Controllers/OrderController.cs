8d1f74e158870b2ff51c68353e806fa30b5ec2eb
Rafael Moreira Pereira <fahelmoreira@me.com>
2018-09-23 08:31:30 -0300

    APIs

---
+++ src/EasyManager.WebAPI/Controllers/OrderController.cs
@@ -0,0 +1,75 @@
+using System;
+using EasyManager.Application.Interfaces;
+using EasyManager.Application.ViewModels;
+using EasyManager.Domain.Core.Bus;
+using EasyManager.Domain.Core.Notifications;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyManager.WebAPI.Controllers
+{
+    [Route("[Controller]")]
+    public class OrderController : ApiController<IOrderAppService>
+    {
+        public OrderController(IOrderAppService appService, 
+                                  INotificationHandler<DomainNotification> notifications, 
+                                  IMediatorHandler mediator) : base(appService, notifications, mediator)
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Response(_appService.GetAll());
+        }
+
+        [HttpGet("{id:Guid}")]
+        public IActionResult Get(Guid id)
+        {
+            return Response(_appService.GetById(id));
+        }
+        
+        [HttpPost]
+        public IActionResult Post([FromBody] OrderViewModel order)
+        {
+            if(!ModelState.IsValid)
+            {
+                NotifyModelStateErrors();
+                return Response(order);
+            }
+
+            _appService.Register(order);
+
+            return Response("Order successfully created");
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] OrderViewModel order)
+        {
+            if(!ModelState.IsValid)
+            {
+                NotifyModelStateErrors();
+                return Response(order);
+            }
+
+            _appService.Update(order);
+
+            return Response("Order successfully updated");
+
+        }
+
+        [HttpDelete]
+        public IActionResult Delete([FromBody] Guid id)
+        {
+            if(!ModelState.IsValid)
+            {
+                NotifyModelStateErrors();
+                return Response(id);
+            }
+
+            _appService.Remove(id);
+
+            return Response("Order successfully removed");
+        }
+    }
+}
---
+++ src/EasyManager.WebAPI/Controllers/PurchaseContoller.cs
@@ -0,0 +1,75 @@
+using System;
+using EasyManager.Application.Interfaces;
+using EasyManager.Application.ViewModels;
+using EasyManager.Domain.Core.Bus;
+using EasyManager.Domain.Core.Notifications;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyManager.WebAPI.Controllers
+{
+    [Route("[Controller]")]
+    public class PurchaseContoller : ApiController<IPurchaseAppService>
+    {
+        public PurchaseContoller(IPurchaseAppService appService, 
+                                    INotificationHandler<DomainNotification> notifications, 
+                                    IMediatorHandler mediator) : base(appService, notifications, mediator)
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Response(_appService.GetAll());
+        }
+
+        [HttpGet("{id:Guid}")]
+        public IActionResult Get(Guid id)
+        {
+            return Response(_appService.GetById(id));
+        }
+        
+        [HttpPost]
+        public IActionResult Post([FromBody] PurchaseViewModel purchase)
+        {
+            if(!ModelState.IsValid)
+            {
+                NotifyModelStateErrors();
+                return Response(purchase);
+            }
+
+            _appService.Register(purchase);
+
+            return Response("Purchase successfully created");
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] PurchaseViewModel purchase)
+        {
+            if(!ModelState.IsValid)
+            {
+                NotifyModelStateErrors();
+                return Response(purchase);
+            }
+
+            _appService.Update(purchase);
+
+            return Response("Purchase successfully updated");
+
+        }
+
+        [HttpDelete]
+        public IActionResult Delete([FromBody] Guid id)
+        {
+            if(!ModelState.IsValid)
+            {
+                NotifyModelStateErrors();
+                return Response(id);
+            }
+
+            _appService.Remove(id);
+
+            return Response("Purchase successfully removed");
+        }
+    }
+}
